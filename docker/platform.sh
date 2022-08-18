63e58f2450196e36f06f6cb51ae59015638dbb1f
sacundim <anybody@sacundim.org>
2022-08-17 21:30:38 -0700

    Working Docker cross-compilation

--- docker/platform.sh
+++ docker/platform.sh
@@ -5,17 +5,12 @@
 # * https://blog.container-solutions.com/building-multiplatform-container-images
 
 case $TARGETARCH in
-
     "amd64")
 	echo "x86_64-unknown-linux-gnu" > /.platform
-	echo "" > /.compiler
+	echo "gcc-x86-64-linux-gnu" > /.compiler
 	;;
     "arm64")
 	echo "aarch64-unknown-linux-gnu" > /.platform
 	echo "gcc-aarch64-linux-gnu" > /.compiler
-	;;
-    "arm")
-	echo "armv7-unknown-linux-gnueabihf" > /.platform
-	echo "gcc-arm-linux-gnueabihf" > /.compiler
 	;;
 esac
